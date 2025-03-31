4811a9e0c499ca09a79b67f11be064589f6bdd8b
new-marty <yumabuchi1998@gmail.com>
2025-03-31 14:48:58 +0900

    update

--- app/routes/__tests__/home.test.tsx
+++ app/routes/__tests__/home.test.tsx
@@ -1,13 +1,13 @@
 import { describe, it, expect } from 'vitest';
 
 describe('Basic tests', () => {
-  // it('should pass a simple equality test', () => {
-  //   expect(1 + 1).toBe(2);
-  // });
-
   it('should pass a simple equality test', () => {
-    expect(1 + 1).toBe(3);
+    expect(1 + 1).toBe(2);
   });
+
+  // it('should pass a simple equality test', () => {
+  //   expect(1 + 1).toBe(3);
+  // });
 
   it('should fail a simple inequality test', () => {
     expect(1 + 1 === 1).toBe(false);
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,8 +1,8 @@
 import type { Route } from './+types/home';
 
 // Lint error here
-export function meta({}: Route.MetaArgs) {
-  // export function meta() {
+// export function meta({}: Route.MetaArgs) {
+export function meta() {
   return [
     { title: 'New Webapp' },
     { name: 'description', content: 'Welcome to New Webapp!' },
